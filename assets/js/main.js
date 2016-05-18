4afb40a91921325d067a1b2971fa4cec2745af86
Harrison Heck <harrison.heck@gmail.com>
2016-05-18 15:43:04 -0400

    Updates

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+malarkey(document.querySelector('.rotating'), {
+    typeSpeed: 50,
+    deleteSpeed: 50,
+    pauseDelay: 2000,
+    loop: true,
+    postfix: ''
+  })
+  .type('Sublime').pause().delete()
+  .type('Atom').pause().delete()
+  .type('Vim').pause().delete()
+  .type('iTerm').pause().delete()
+  .type('Terminal.app').pause().delete()
+  .type('Zsh').pause().delete()
+  .type('Alfred').pause().delete()
+  .type('Slack').pause().delete()
+  .type('CodeMirror').pause().delete()
+  .type('Xcode').pause().delete()
+  .type('Coda').pause().delete()
+  .type('TextMate').pause().delete()
+  .type('Emacs').pause().delete()
+  .type('Sequel Pro').pause().delete()
+  .type('Visual Studio Code').pause().delete()
+  .type('JetBrains').pause().delete()
+  .type('Visual Studio').pause().delete()
