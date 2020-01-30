acd41a0fb4c40c630dd5665ed9331a813731a2a1
Zeno Rocha <zno.rocha@gmail.com>
2020-01-29 16:11:04 -0800

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8d4a01134512731e6862";
+/******/ 	var hotCurrentHash = "cb177850f553b0fb2d52";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
