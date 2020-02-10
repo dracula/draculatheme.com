5cee143fe03455ad569be906b5adefcc7afd40cf
Zeno Rocha <zno.rocha@gmail.com>
2020-02-10 15:28:55 -0800

    Updates

---
+++ _next/static/Koe1Nv9g4Iz8Tg8RgrzZ-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/[theme]":[a],"/about":[a],"/index":[a,b],"/pro":[b]}}("static\u002Fchunks\u002Fba264d9c03c527509b679b86e56982d366a3cb6f.9e5aa16230b6d06fe4af.js","static\u002Fchunks\u002Faaaa68b873a8b477f9778bb928bd71f033331333.1d89e590ceaf185e2202.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
