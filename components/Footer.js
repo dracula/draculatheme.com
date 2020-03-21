8ce0f3fa37e41275d6d0df38a08222631f1b997a
Zeno Rocha <zno.rocha@gmail.com>
2020-03-21 14:18:55 -0700

    Fix footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,9 @@ import { Component } from 'react';
 
 class Footer extends Component {
   render() {
-    return <p className="credits">Made with <span className="love">♥</span> by <a className="green" href="https://zenorocha.com" target="blank">Zeno Rocha</a><br />under <a className="orange" href="http://zenorocha.mit-license.org/" target="blank">MIT license</a></p>
+    return <div style={{ background: '#282a36', padding: '60px 0' }}>
+      <p className="credits">Made with <span className="love">♥</span> by <a className="green" href="https://zenorocha.com" target="blank">Zeno Rocha</a><br />under <a className="orange" href="http://zenorocha.mit-license.org/" target="blank">MIT license</a></p>
+    </div>
   }
 }
 
