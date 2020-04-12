98d2f3223b009a6cb225dac3b456bb329399a4eb
Zeno Rocha <zno.rocha@gmail.com>
2020-04-11 20:54:20 -0700

    Fix PRO header

--- components/pro/Header.js
+++ components/pro/Header.js
@@ -4,8 +4,8 @@ import styles from './Header.module.css';
 class Header extends Component {
   render() {
     return <div className={styles.header}>
-      <h1 className={styles.title}>{this.props.title}</h1>
-      <h2 className={styles.slogan}>{this.props.description}</h2>
+      <h1 className={styles.title}>Dracula PRO</h1>
+      <h2 className={styles.slogan}>Be more productive</h2>
       <a href="#get" className={styles.cta}>Get it now</a>
     </div>
   }
