256b7164ebb680a7e6231d0b465c4ae4794c1cd9
Zeno Rocha <zno.rocha@gmail.com>
2020-04-14 14:12:50 -0700

    Add Twitter tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -37,6 +37,15 @@ export default class extends Document {
           <noscript dangerouslySetInnerHTML={{ __html: `
             <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1094357704254738&ev=PageView&noscript=1" />
           `}} />
+
+          <script dangerouslySetInnerHTML={{ __html: `
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            // Insert Twitter Pixel ID and Standard Event data below
+            twq('init','o3jao');
+            twq('track','PageView');
+          `}} />
         </Head>
         <Main />
         <NextScript />
