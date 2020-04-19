5c686c41e3de7cb27e7d29da8faff36ea6f0056c
Zeno Rocha <zno.rocha@gmail.com>
2020-04-19 08:16:30 -0700

    Remove FB and TW in favor of GTM

--- pages/_document.js
+++ pages/_document.js
@@ -23,30 +23,17 @@ export default class extends Document {
           `}} />
 
           <script dangerouslySetInnerHTML={{ __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1094357704254738');
-            fbq('track', 'PageView');
-          `}} />
-          <noscript dangerouslySetInnerHTML={{ __html: `
-            <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1094357704254738&ev=PageView&noscript=1" />
-          `}} />
-
-          <script dangerouslySetInnerHTML={{ __html: `
-            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
-            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-            // Insert Twitter Pixel ID and Standard Event data below
-            twq('init','o3jao');
-            twq('track','PageView');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KMBFFLG');
           `}} />
         </Head>
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMBFFLG"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
         <Main />
         <NextScript />
       </html>
