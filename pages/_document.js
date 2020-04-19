87b9e1ceb4490c2c91108af82849e82a760130be
Zeno Rocha <zno.rocha@gmail.com>
2020-04-19 08:23:35 -0700

    Fix <noscript> insert

--- pages/_document.js
+++ pages/_document.js
@@ -31,8 +31,9 @@ export default class extends Document {
           `}} />
         </Head>
 
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMBFFLG"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript dangerouslySetInnerHTML={{ __html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMBFFLG" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        `}} />
 
         <Main />
         <NextScript />
