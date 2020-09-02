fae74c93e474ecde6e71f73982a711722c75bf7b
Zeno Rocha <zno.rocha@gmail.com>
2020-09-02 00:25:43 -0700

    Add Dracula UI page and banner

--- components/Topbar.js
+++ components/Topbar.js
@@ -3,29 +3,38 @@ import Link from 'next/link';
 import styles from './Topbar.module.css';
 
 class Topbar extends Component {
+  renderBanner() {
+    return <a className={styles.banner} href="/ui" target="_blank">
+      <p className={styles.bannerContent}>Coming Soon // Dracula UI // Join the Waitlist</p>
+    </a>
+  }
+
   render() {
-    return <nav className={styles.topbar}>
-      <Link href="/">
-        <a className="topbar-title">Dracula</a>
-      </Link>
-      <ul>
-        <li className={styles.about}>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li className={styles.contribute}>
-          <Link href="/contribute">
-            <a>Contribute</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/pro">
-            <a className={styles.cta}>Dracula PRO</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    return <div className={styles.fixed}>
+      {this.renderBanner()}
+      <nav className={styles.topbar}>
+        <Link href="/">
+          <a className="topbar-title">Dracula</a>
+        </Link>
+        <ul>
+          <li className={styles.about}>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className={styles.contribute}>
+            <Link href="/contribute">
+              <a>Contribute</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/pro">
+              <a className={styles.cta}>Dracula PRO</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   }
 }
 
---
+++ pages/ui.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './ui.module.css';
+
+class UI extends React.Component {
+  static async getInitialProps() {
+    const query = {
+      color: 'green',
+      icon: 'dracula.svg'
+    };
+
+    return { query };
+  }
+
+  render() {
+    const title = 'Dracula UI';
+    const description = 'A dark-first collection of UI patterns and components';
+
+    return (
+      <div className={styles.wrapper}>
+        <Head>
+          <meta charSet="utf-8" />
+          <title>{title} &mdash; {description}</title>
+          <meta content={title} property="og:title" />
+          <meta content={description} name="description" />
+          <meta content={description} property="og:description" />
+          <meta content="https://draculatheme.com/ui" property="og:url" />
+          <meta content="https://draculatheme.com/static/img/facebook.png" property="og:image" />
+        </Head>
+
+        <div className={styles.header}>
+          <p className={styles.comingSoon}>Coming Soon</p>
+          <h1 className={styles.title}>Dracula <span className={styles.purple}>UI</span></h1>
+          <p className={styles.subtitle}>A dark-first collection of <span className={styles.purple}>UI patterns and components</span></p>
+        </div>
+
+        <div className={styles.body}>
+          <h2 className={styles.bodyTitle}>Join the Waitlist</h2>
+          <p className={styles.bodyText}>Be among the first to build stunning dark mode apps with Dracula UI.</p>
+          <div className={styles.bodyForm}>
+            <form className="form" action="https://draculatheme.us4.list-manage.com/subscribe/post?u=91b5113403e18d357704e4b08&amp;id=023365a1d4" method="post">
+              <input name="EMAIL" placeholder="your@email.com" id="mce-EMAIL" type="email" required />
+              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
+            </form>
+          </div>
+        </div>
+
+        <div className={styles.footer}>
+          <p style={{ fontFamily: 'Fira Code, monospace', fontSize: 16, margin: 0 }} className="credits">Made with <span style={{ fontSize: 36 }} className="love">♥</span> by <a className="cyan" href="https://zenorocha.com" target="blank">Zeno Rocha</a> & <a className="cyan" href="https://twitter.com/nettofarah" target="blank">Netto Farah</a></p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default UI;
