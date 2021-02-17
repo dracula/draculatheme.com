449250c225c5ca03c5a9ae9b5d1591c3b17c76fd
Zeno Rocha <zno.rocha@gmail.com>
2021-02-17 11:04:58 -0800

    Add anchor to each testimonial

--- components/pro/Reviews.js
+++ components/pro/Reviews.js
@@ -7,9 +7,17 @@ class Reviews extends Component {
     super(props);
 
     this.state = {
-      showAllButton: true,
-      reviews: props.reviews.slice(0, 3)
+      reviews: props.reviews
     };
+  }
+
+  componentDidMount() {
+    if (window.location.hash.indexOf('#review') === -1) {
+      this.setState({
+        showAllButton: true,
+        reviews: this.props.reviews.slice(0, 3)
+      });
+    }
   }
 
   showAll(e) {
@@ -102,22 +110,23 @@ class Reviews extends Component {
   renderReviews() {
     return <div>
       {this.state.reviews.map(review => {
-        return <div key={review.id} className={styles.review}>
-          <div>
+        return <div id={`review-${review.github}`} key={review.id} className={styles.review}>
+          <div className={styles.avatarContainer}>
+            <a className={styles.anchor} href={`#review-${review.github}`}>
+              <svg className={styles.anchorIcon} viewBox="0 0 16 16" version="1.1" width="24" height="24" aria-hidden="true"><path fillRule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg>
+            </a>
             <img className={styles.avatar} src={`https://github.com/${review.github}.png?size=140`} alt={review.name} width="70" height="70" />
           </div>
           <div>
             <div className={styles.name}>{review.name}
-              {review.country &&
-                <img className={styles.country} src={`/static/img/flags/${review.country}.svg`} alt={review.country} width="100" />
-              }
+              <img className={styles.country} src={`/static/img/flags/${review.country}.svg`} alt={review.country} width="100" />
             </div>
             <div className={styles.date}>{moment(review.date).fromNow()}</div>
             <div className={styles.body}>{review.body}</div>
           </div>
-        </div>
+        </div >
       })}
-    </div>
+    </div >
   }
 
   render() {
