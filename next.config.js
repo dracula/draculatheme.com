8c8db2926785d8effbaadc790312bae91ce9305c
Zeno Rocha <zno.rocha@gmail.com>
2022-01-29 11:52:29 -0800

    Upgrade to Next 12

--- next.config.js
+++ next.config.js
@@ -5,4 +5,6 @@ if (!process.env.GITHUB_PERSONAL_ACCESS_TOKEN) {
 
 const withTM = require('next-transpile-modules')(['react-github-btn']);
 
-module.exports = withTM();
+module.exports = withTM({
+  swcMinify: true
+});
