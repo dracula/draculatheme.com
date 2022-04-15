cab11d9533aa51e862be03afcec91c24daad6f88
Zeno Rocha <zno.rocha@gmail.com>
2022-04-15 15:39:01 -0700

    Add Cache Control headers to API requests

--- pages/api/installs/[id].js
+++ pages/api/installs/[id].js
@@ -5,6 +5,10 @@ export default async ({ query: { id } }, res) => {
     const value = await redis.hget('installs', id)
     const install = value || ''
 
+    res.setHeader(
+      'Cache-Control',
+      'public, s-maxage=1200, stale-while-revalidate=600'
+    )
     return res.status(200).json({ install })
   } catch (error) {
     return res.status(400).json({ install: '' })
--- pages/api/views/[id].js
+++ pages/api/views/[id].js
@@ -5,6 +5,10 @@ export default async ({ query: { id } }, res) => {
     const value = await redis.hget('views', id)
     const views = parseInt(value, 10) || 0
 
+    res.setHeader(
+      'Cache-Control',
+      'public, s-maxage=1200, stale-while-revalidate=600'
+    )
     return res.status(200).json({ views })
   } catch (error) {
     return res.status(400).json({ views: 0 })
