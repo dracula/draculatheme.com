c7a68a59ea28551c2c1d95acff7c1ba81c53c484
Lucas de França <luxonauta@gmail.com>
2022-04-25 14:27:50 -0300

    :sparkles: Add Contentlayer and some tweaks

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 if (!process.env.GITHUB_PERSONAL_ACCESS_TOKEN) {
   throw 'Error: You need to define a GitHub Personal Access Token to run the site, see README for more info'
-  return
 }
 
+const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(['react-github-btn'])
+const { withContentlayer } = require('next-contentlayer')
 
-module.exports = withTM({
+module.exports = withPlugins([withTM, withContentlayer], {
   swcMinify: true,
   async redirects() {
     return [
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,90 +1,80 @@
-import Blogpost from '../../layouts/Blogpost';
-import Updates from '../../components/Updates';
-import BlogDate from '../../components/BlogDate';
-import { getAllPosts } from '../../lib/blog';
-import { getBasePath } from '../../lib/environment';
-import Head from 'next/head';
-import Link from 'next/link';
+import { allBlogPosts } from 'contentlayer/generated'
+import { format, parseISO } from 'date-fns'
+import { getBasePath } from '../../lib/environment'
+import Blogpost from '../../layouts/Blogpost'
+import Head from 'next/head'
+import Link from 'next/link'
+import Updates from '../../components/Updates'
 
 export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'createdAt',
-    'slug',
-    'author',
-    'excerpt',
-    'color',
-  ])
+  const posts = allBlogPosts.sort((post1, post2) =>
+    post1.createdAt > post2.createdAt ? '-1' : '1'
+  )
 
-  const totalSubscribersReq = await fetch(`${getBasePath()}/api/mailchimp`);
-  const totalSubscribersRes = await totalSubscribersReq.json();
-  const totalSubscribers = totalSubscribersRes.total;
+  const totalSubscribersReq = await fetch(`${getBasePath()}/api/mailchimp`)
+  const totalSubscribersRes = await totalSubscribersReq.json()
+  const totalSubscribers = totalSubscribersRes.total
 
   return {
-    props: {
-      allPosts,
-      totalSubscribers,
-      post: { color: 'yellow' }
-    },
-    revalidate: 7200
+    props: { posts, totalSubscribers, post: { color: 'yellow' } },
+    revalidate: 7200,
   }
 }
 
-function Blog({ allPosts, totalSubscribers }) {
-  const title = 'Blog — Dracula Theme';
-  const description = 'The journey of building the most universal dark theme ever made.';
-  const image = '/static/img/facebook.png';
+export default function Blog({ posts, totalSubscribers }) {
+  const title = 'Blog — Dracula Theme'
+  const description =
+    'The journey of building the most universal dark theme ever made.'
+  const image = '/static/img/facebook.png'
 
-	return (
-		<div className="single">
-			<Head>
-				<meta charSet="utf-8" />
-				<title>{title}</title>
-				<meta content={title} property="og:title" />
-				<meta content={description} name="description" />
-				<meta content={description} property="og:description" />
-				<meta content="Zeno Rocha" name="author" />
-				<meta content="https://draculatheme.com/blog" property="og:url" />
-				<meta content={`https://draculatheme.com${image}`} property="og:image" />
-			</Head>
+  return (
+    <div className="single">
+      <Head>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <meta content={title} property="og:title" />
+        <meta content={description} name="description" />
+        <meta content={description} property="og:description" />
+        <meta content="Zeno Rocha" name="author" />
+        <meta content="https://draculatheme.com/blog" property="og:url" />
+        <meta
+          content={`https://draculatheme.com${image}`}
+          property="og:image"
+        />
+      </Head>
 
-			<div className="wrap">
-				<div className="blog">
-					{allPosts.map((post) => {
-						return (
-							<div className={post.color} key={post.slug}>
-								<div className="blog-item">
-									<div className="blog-item-header">
-										<Link href={`/blog/${post.slug}`}>
-											<a>
-												<h2 className="blog-item-title">{post.title}</h2>
-											</a>
-										</Link>
-										<span className="blog-item-date">
-											<BlogDate dateString={post.createdAt} />
-										</span>
-									</div>
-									<div
-										className="blog-item-excerpt"
-										dangerouslySetInnerHTML={{ __html: post.excerpt }}
-									/>
-								</div>
-							</div>
-						);
-					})}
-				</div>
-			</div>
+      <div className="wrap">
+        <div className="blog">
+          {posts.map((post, idx) => (
+            <div className={post.color} key={idx}>
+              <div className="blog-item">
+                <div className="blog-item-header">
+                  <Link href={post.url}>
+                    <a>
+                      <h2 className="blog-item-title">{post.title}</h2>
+                    </a>
+                  </Link>
+                  <span className="blog-item-date">
+                    <time dateTime={post.createdAt}>
+                      {format(parseISO(post.createdAt), 'LLLL d, yyyy')}
+                    </time>
+                  </span>
+                </div>
+                <div
+                  className="blog-item-excerpt"
+                  dangerouslySetInnerHTML={{ __html: post.excerpt }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
 
       <div className="blog-updates">
-        <Updates
-          type="blog"
-          totalSubscribers={totalSubscribers}
-        />
+        <Updates type="blog" totalSubscribers={totalSubscribers} />
       </div>
     </div>
   )
 }
 
-Blog.Layout = Blogpost;
-
-export default Blog;
+Blog.Layout = Blogpost
