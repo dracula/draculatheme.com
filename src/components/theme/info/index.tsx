8eaf6f0fcb5969850e59d734ac427c420980df8b
Zeno Rocha <zno.rocha@gmail.com>
2023-10-11 20:12:19 -0700

    ✨ Add theme details

--- src/components/theme/info/index.tsx
+++ src/components/theme/info/index.tsx
@@ -1,11 +1,11 @@
 import Contributor from "../contributor";
-import { GithubIcon } from "lucide-react";
+import { GithubIcon, PenSquareIcon, FolderDownIcon, BugIcon } from "lucide-react";
 import Link from "next/link";
 
 const Info = ({ theme, contributors }) => (
   <div className="info">
     <div className="item">
-      <span className="title">Source code</span>
+      <span className="title">Details</span>
       <Link
         href={`https://github.com/dracula/${theme}`}
         target="_blank"
@@ -14,7 +14,37 @@ const Info = ({ theme, contributors }) => (
         <span className="icon">
           <GithubIcon />
         </span>
-        <span>View source code.</span>
+        <span>View source code</span>
+      </Link>
+      <Link
+        href={`https://github.com/dracula/${theme}/archive/refs/heads/master.zip`}
+        target="_blank"
+        className="inline"
+      >
+        <span className="icon">
+          <FolderDownIcon />
+        </span>
+        <span>Download ZIP file</span>
+      </Link>
+      <Link
+        href={`https://github.com/dracula/${theme}/issues/new`}
+        target="_blank"
+        className="inline"
+      >
+        <span className="icon">
+          <BugIcon />
+        </span>
+        <span>Report an issue</span>
+      </Link>
+      <Link
+        href={`https://github.com/dracula/${theme}/edit/master/README.md`}
+        target="_blank"
+        className="inline"
+      >
+        <span className="icon">
+          <PenSquareIcon />
+        </span>
+        <span>Edit this page</span>
       </Link>
     </div>
     <div className="item">
