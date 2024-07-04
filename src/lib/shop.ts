964b5690efc4b7bac31a47551662fe04528d8e19
Lucas de França <luxon4uta@gmail.com>
2024-07-04 03:16:30 -0300

    🌕 Add poster

--- src/lib/shop.ts
+++ src/lib/shop.ts
@@ -1,35 +1,22 @@
 const products = [
   {
     params: {
-      slug: "periodic-table-shirt",
-      gumroadId: "qyxgtx",
-      images: [
-        "periodic-table-shirt-1.png",
-        "periodic-table-shirt-2.png",
-        "periodic-table-shirt-3.png",
-        "periodic-table-shirt-4.png"
-      ],
-      size: "#shirts",
-      defaultVariant: 2,
-      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
-      category: "shirts",
-      color: "cyan"
+      slug: "alucard-poster",
+      gumroadId: "ahnayk",
+      images: ["alucard-poster-1.png", "alucard-poster-2.png"],
+      category: "other",
+      color: "purple"
     }
   },
   {
     params: {
-      slug: "dracula-hoodie",
-      gumroadId: "jeaaum",
-      images: [
-        "dracula-hoodie-1.png",
-        "dracula-hoodie-2.png",
-        "dracula-hoodie-3.png",
-        "dracula-hoodie-4.png"
-      ],
-      size: "#hoodies",
-      defaultVariant: 1,
-      variants: ["S", "M", "L", "XL", "2XL", "3XL"],
-      category: "other",
+      slug: "ascii-shirt",
+      gumroadId: "hjppo",
+      images: ["ascii-shirt-1.png"],
+      size: "#shirts",
+      defaultVariant: 2,
+      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
+      category: "shirts",
       color: "green"
     }
   },
@@ -47,6 +34,22 @@ const products = [
       variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
       category: "shirts",
       color: "purple"
+    }
+  },
+  {
+    params: {
+      slug: "dark-mode-hoodie",
+      gumroadId: "vuine",
+      images: [
+        "dark-mode-hoodie-1.png",
+        "dark-mode-hoodie-2.png",
+        "dark-mode-hoodie-3.png"
+      ],
+      size: "#hoodies",
+      defaultVariant: 1,
+      variants: ["S", "M", "L", "XL", "2XL", "3XL"],
+      category: "other",
+      color: "yellow"
     }
   },
   {
@@ -74,18 +77,31 @@ const products = [
   },
   {
     params: {
-      slug: "dracula-sticker-pack-n1",
-      gumroadId: "cntzy",
+      slug: "dracula-hoodie",
+      gumroadId: "jeaaum",
       images: [
-        "dracula-sticker-pack-n1-1.png",
-        "dracula-sticker-pack-n1-2.png",
-        "dracula-sticker-pack-n1-3.png",
-        "dracula-sticker-pack-n1-4.png",
-        "dracula-sticker-pack-n1-5.png",
-        "dracula-sticker-pack-n1-6.png"
+        "dracula-hoodie-1.png",
+        "dracula-hoodie-2.png",
+        "dracula-hoodie-3.png",
+        "dracula-hoodie-4.png"
       ],
-      category: "stickers",
-      color: "orange"
+      size: "#hoodies",
+      defaultVariant: 1,
+      variants: ["S", "M", "L", "XL", "2XL", "3XL"],
+      category: "other",
+      color: "green"
+    }
+  },
+  {
+    params: {
+      slug: "dracula-jogger",
+      gumroadId: "vmywr",
+      images: ["dracula-jogger-1.png"],
+      size: "#joggers",
+      defaultVariant: 2,
+      variants: ["XS", "S", "M", "L", "XL", "2XL"],
+      category: "other",
+      color: "cyan"
     }
   },
   {
@@ -95,94 +111,6 @@ const products = [
       images: ["dracula-pin-n1-1.png"],
       category: "other",
       color: "purple"
-    }
-  },
-  {
-    params: {
-      slug: "hexadecimal-shirt",
-      gumroadId: "qfbzl",
-      images: ["hexadecimal-shirt-1.png"],
-      size: "#shirts",
-      defaultVariant: 2,
-      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
-      category: "shirts",
-      color: "purple"
-    }
-  },
-  {
-    params: {
-      slug: "ascii-shirt",
-      gumroadId: "hjppo",
-      images: ["ascii-shirt-1.png"],
-      size: "#shirts",
-      defaultVariant: 2,
-      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
-      category: "shirts",
-      color: "green"
-    }
-  },
-  {
-    params: {
-      slug: "love-wins-shirt",
-      gumroadId: "tjqktv",
-      images: ["love-wins-shirt-1.png"],
-      size: "#shirts",
-      defaultVariant: 2,
-      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
-      category: "shirts",
-      color: "cyan"
-    }
-  },
-  {
-    params: {
-      slug: "vampire-slayers-shirt",
-      gumroadId: "dphwp",
-      images: ["vampire-slayers-shirt-1.png"],
-      size: "#shirts",
-      defaultVariant: 2,
-      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
-      category: "shirts",
-      color: "orange"
-    }
-  },
-  {
-    params: {
-      slug: "minimalist-pullover",
-      gumroadId: "syitd",
-      images: ["minimalist-pullover-1.png", "minimalist-pullover-2.png"],
-      size: "#pullovers",
-      defaultVariant: 1,
-      variants: ["S", "M", "L", "XL", "2XL", "3XL"],
-      category: "other",
-      color: "green"
-    }
-  },
-  {
-    params: {
-      slug: "little-vampire-onesie",
-      gumroadId: "fuoifx",
-      images: ["little-vampire-onesie-1.png", "little-vampire-onesie-2.png"],
-      size: "#onesies",
-      defaultVariant: 0,
-      variants: ["3-6m", "6-12m", "12-18m", "18-24m"],
-      category: "other",
-      color: "pink"
-    }
-  },
-  {
-    params: {
-      slug: "dark-mode-hoodie",
-      gumroadId: "vuine",
-      images: [
-        "dark-mode-hoodie-1.png",
-        "dark-mode-hoodie-2.png",
-        "dark-mode-hoodie-3.png"
-      ],
-      size: "#hoodies",
-      defaultVariant: 1,
-      variants: ["S", "M", "L", "XL", "2XL", "3XL"],
-      category: "other",
-      color: "yellow"
     }
   },
   {
@@ -201,14 +129,18 @@ const products = [
   },
   {
     params: {
-      slug: "dracula-jogger",
-      gumroadId: "vmywr",
-      images: ["dracula-jogger-1.png"],
-      size: "#joggers",
-      defaultVariant: 2,
-      variants: ["XS", "S", "M", "L", "XL", "2XL"],
-      category: "other",
-      color: "cyan"
+      slug: "dracula-sticker-pack-n1",
+      gumroadId: "cntzy",
+      images: [
+        "dracula-sticker-pack-n1-1.png",
+        "dracula-sticker-pack-n1-2.png",
+        "dracula-sticker-pack-n1-3.png",
+        "dracula-sticker-pack-n1-4.png",
+        "dracula-sticker-pack-n1-5.png",
+        "dracula-sticker-pack-n1-6.png"
+      ],
+      category: "stickers",
+      color: "orange"
     }
   },
   {
@@ -218,6 +150,83 @@ const products = [
       images: ["dracula-stickers-1.png"],
       category: "stickers",
       color: "pink"
+    }
+  },
+  {
+    params: {
+      slug: "hexadecimal-shirt",
+      gumroadId: "qfbzl",
+      images: ["hexadecimal-shirt-1.png"],
+      size: "#shirts",
+      defaultVariant: 2,
+      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
+      category: "shirts",
+      color: "purple"
+    }
+  },
+  {
+    params: {
+      slug: "little-vampire-onesie",
+      gumroadId: "fuoifx",
+      images: ["little-vampire-onesie-1.png", "little-vampire-onesie-2.png"],
+      size: "#onesies",
+      defaultVariant: 0,
+      variants: ["3-6m", "6-12m", "12-18m", "18-24m"],
+      category: "other",
+      color: "pink"
+    }
+  },
+  {
+    params: {
+      slug: "love-wins-shirt",
+      gumroadId: "tjqktv",
+      images: ["love-wins-shirt-1.png"],
+      size: "#shirts",
+      defaultVariant: 2,
+      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
+      category: "shirts",
+      color: "cyan"
+    }
+  },
+  {
+    params: {
+      slug: "minimalist-pullover",
+      gumroadId: "syitd",
+      images: ["minimalist-pullover-1.png", "minimalist-pullover-2.png"],
+      size: "#pullovers",
+      defaultVariant: 1,
+      variants: ["S", "M", "L", "XL", "2XL", "3XL"],
+      category: "other",
+      color: "green"
+    }
+  },
+  {
+    params: {
+      slug: "periodic-table-shirt",
+      gumroadId: "qyxgtx",
+      images: [
+        "periodic-table-shirt-1.png",
+        "periodic-table-shirt-2.png",
+        "periodic-table-shirt-3.png",
+        "periodic-table-shirt-4.png"
+      ],
+      size: "#shirts",
+      defaultVariant: 2,
+      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
+      category: "shirts",
+      color: "cyan"
+    }
+  },
+  {
+    params: {
+      slug: "vampire-slayers-shirt",
+      gumroadId: "dphwp",
+      images: ["vampire-slayers-shirt-1.png"],
+      size: "#shirts",
+      defaultVariant: 2,
+      variants: ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"],
+      category: "shirts",
+      color: "orange"
     }
   }
 ];
