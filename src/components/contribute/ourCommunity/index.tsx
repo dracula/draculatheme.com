a26866013037b7a44e57fc8e11bcbc31d33adcec
Lucas de França <luxon4uta@gmail.com>
2024-07-15 16:09:57 -0300

    📕 Copy changes

--- src/components/contribute/ourCommunity/index.tsx
+++ src/components/contribute/ourCommunity/index.tsx
@@ -18,12 +18,12 @@ const AvatarItem = ({ url, name, src }) => (
 const OurCommunity = ({ contributorsList }) => {
   return (
     <div id="our-community">
-      <h3>Our Community!</h3>
+      <h3>Our Community</h3>
       <div className="text">
         <p>
-          Whether you&apos;re maintaining your port, contributing via a pull
+          Whether you&apos;re maintaining a port, contributing via a pull
           request, or reporting an issue, the community is the heart and soul of
-          Dracula Theme.
+          the Dracula Theme.
         </p>
       </div>
       <ul id="contributors-list">
