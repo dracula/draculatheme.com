1a758941b7a129532b0f09b6a9b715e18a857770
Lucas de França <luxon4uta@gmail.com>
2024-08-09 12:34:43 -0300

    📕 Update copy

--- src/components/pro/contentWrapper/alucard/index.tsx
+++ src/components/pro/contentWrapper/alucard/index.tsx
@@ -7,18 +7,17 @@ const Alucard = () => {
     <article className="alucard">
       <CardPlain>
         <div className="col content">
-          <span className="title s">Our First Light Theme</span>
+          <span className="title s">The Official Light Theme</span>
           <div className="text">
             <p>
               For years, we resisted the idea of a light theme for Dracula.
-              However, we recognized its importance for accessibility and use in
+              However, we recognize its importance for accessibility and use in
               bright environments.
             </p>
             <p>
-              Alucard, a light variant that seamlessly integrates with Dracula
-              PRO, was born. It meets WCAG AA contrast standards, a testament to
-              our careful development process that ensures legibility and
-              inclusivity.
+              That&apos;s why we created Alucard, a light variant that
+              seamlessly integrates with Dracula PRO. It meets WCAG AA contrast
+              standards, ensuring legibility and inclusivity.
             </p>
           </div>
         </div>
