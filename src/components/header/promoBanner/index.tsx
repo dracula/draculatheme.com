e645ffa3a719fa9863fa547004751eacbac89761
Lucas de França <luxon4uta@gmail.com>
2024-09-03 10:18:57 -0300

    🤲🏻 Up banner with fundraising link

--- src/components/header/promoBanner/index.tsx
+++ src/components/header/promoBanner/index.tsx
@@ -1,12 +1,18 @@
 import "./index.scss";
+import { ChevronRightIcon } from "lucide-react";
 import Link from "next/link";
 
 const PromoBanner = () => {
   return (
-    <Link href="/pro" className="promo-banner">
-      <div className="container">
-        <span>Alucard has arrived. Meet the Dracula light theme.</span>
-      </div>
+    <Link
+      href="https://codinginpublic.dev/fundraiser?ref=draculatheme.com"
+      className="promo-banner"
+    >
+      <span className="container">
+        Part of September&apos;s purchases will go towards curing childhood
+        cancer. <i>Read more </i>
+        <ChevronRightIcon size={12} strokeWidth={3} className="icon" />
+      </span>
     </Link>
   );
 };
