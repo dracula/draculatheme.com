156d7741c087542115f1b374ef5094b07a31dab8
Lucas de França <luxon4uta@gmail.com>
2025-04-15 16:51:53 -0300

    🪚 Smol visual fixes

--- src/components/pro/codeRain/index.tsx
+++ src/components/pro/codeRain/index.tsx
@@ -4,61 +4,61 @@ import "./index.scss";
 import React, { useEffect, useRef } from "react";
 
 const MatrixRain = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    const cnvs = canvasRef.current;
-    const cntxt = cnvs.getContext("2d");
-    let chars: string[] =
-      "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑ॐ".split(
-        ""
-      );
-    const font_size = 14;
+	useEffect(() => {
+		const cnvs = canvasRef.current;
+		const cntxt = cnvs.getContext("2d");
+		const chars: string[] =
+			"田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑ॐ".split(
+				"",
+			);
+		const font_size = 14;
 
-    const resizeCanvas = () => {
-      cnvs.width = window.innerWidth;
-      setTimeout(() => {
-        cnvs.height = window.innerHeight;
-      }, 0);
-    };
+		const resizeCanvas = () => {
+			cnvs.width = window.innerWidth;
+			setTimeout(() => {
+				cnvs.height = window.innerHeight;
+			}, 0);
+		};
 
-    window.addEventListener("resize", resizeCanvas);
-    resizeCanvas();
+		window.addEventListener("resize", resizeCanvas);
+		resizeCanvas();
 
-    const columns = Math.max(0, Math.floor(cnvs.width / font_size));
-    const drops = Array(columns).fill(1);
+		const columns = Math.max(0, Math.floor(cnvs.width / font_size));
+		const drops = Array(columns).fill(1);
 
-    const draw = () => {
-      cntxt.fillStyle = "rgba(0,0,0,0.05)";
-      cntxt.fillRect(0, 0, cnvs.width, cnvs.height);
-      cntxt.fillStyle = "#0F0";
-      cntxt.font = `${font_size}px helvetica`;
+		const draw = () => {
+			cntxt.fillStyle = "rgba(0,0,0,0.05)";
+			cntxt.fillRect(0, 0, cnvs.width, cnvs.height);
+			cntxt.fillStyle = "#0F0";
+			cntxt.font = `${font_size}px helvetica`;
 
-      for (let i = 0; i < drops.length; i++) {
-        const txt = chars[Math.floor(Math.random() * chars.length)];
-        cntxt.fillText(txt, i * font_size, drops[i] * font_size);
+			for (let i = 0; i < drops.length; i++) {
+				const txt = chars[Math.floor(Math.random() * chars.length)];
+				cntxt.fillText(txt, i * font_size, drops[i] * font_size);
 
-        if (drops[i] * font_size > cnvs.height && Math.random() > 0.975) {
-          drops[i] = 0;
-        }
+				if (drops[i] * font_size > cnvs.height && Math.random() > 0.975) {
+					drops[i] = 0;
+				}
 
-        drops[i]++;
-      }
-    };
+				drops[i]++;
+			}
+		};
 
-    const intervalId = setInterval(draw, 32);
+		const intervalId = setInterval(draw, 32);
 
-    return () => {
-      clearInterval(intervalId);
-      window.removeEventListener("resize", resizeCanvas);
-    };
-  }, []);
+		return () => {
+			clearInterval(intervalId);
+			window.removeEventListener("resize", resizeCanvas);
+		};
+	}, []);
 
-  return (
-    <div className="matrix-rain">
-      <canvas ref={canvasRef}></canvas>
-    </div>
-  );
+	return (
+		<div className="matrix-rain">
+			<canvas ref={canvasRef} />
+		</div>
+	);
 };
 
 export default MatrixRain;
