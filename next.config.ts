3ab076899453fc8b0b93ee9b7f50901d0eb6c26c
Lucas de França <luxon4uta@gmail.com>
2025-08-06 16:48:57 -0300

    🥕 update redirects

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,26 @@ const nextConfig: NextConfig = {
         hostname: "raw.githubusercontent.com"
       }
     ]
+  },
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+  async redirects() {
+    return [
+      {
+        source: "/discord-invite",
+        destination: "https://discord.gg/yDcFsrYuq9",
+        permanent: true
+      },
+      {
+        source: "/ui",
+        destination: "https://ui.draculatheme.com",
+        permanent: true
+      },
+      {
+        source: "/shop/dracula-sticker-pack",
+        destination: "https://draculatheme.com/shop/dracula-sticker-pack-n1",
+        permanent: true
+      }
+    ];
   }
 };
 
