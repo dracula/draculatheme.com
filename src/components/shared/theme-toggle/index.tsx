34bd1d6b9f61150cd6a07c1c46419b3eb0348b2e
luxonauta <luxon4uta@gmail.com>
2025-09-19 14:28:03 -0300

    🌅 new illustrations

--- src/components/shared/theme-toggle/index.tsx
+++ src/components/shared/theme-toggle/index.tsx
@@ -2,16 +2,23 @@
 
 import "./index.css";
 
+import { useSound } from "use-sound";
+
 import { useThemeStore } from "@/store/theme";
 
 export const ThemeToggle = () => {
   const currentTheme = useThemeStore((s) => s.currentTheme);
+  const [play] = useSound("/sounds/toggle.mp3");
+
   const toggleTheme = useThemeStore((s) => s.toggleTheme);
 
   return (
     <button
       type="button"
-      onClick={toggleTheme}
+      onClick={() => {
+        play();
+        toggleTheme();
+      }}
       className="action theme-toggle"
       aria-label="Toggle Theme"
       title="Toggle Theme"
