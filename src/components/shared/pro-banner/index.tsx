811b22fc9d91c4f2f70b57aae196795a49d4383c
luxonauta <luxon4uta@gmail.com>
2025-10-23 20:28:07 -0300

    🔩 smol fix

--- src/components/shared/pro-banner/index.tsx
+++ src/components/shared/pro-banner/index.tsx
@@ -3,15 +3,12 @@ import "./index.css";
 import Link from "next/link";
 
 import { ArrowUpRight } from "@/icons/arrow-up-right";
-import { CheckmarkBadge } from "@/icons/checkmark-badge";
 
 export const ProBanner = () => (
   <Link href="/pro" className="pro-banner">
     <ArrowUpRight size={14} className="arrow" />
     <span>Invest in Your Productivity.</span>
-    <h3>
-      <CheckmarkBadge size={18} className="checkmark" /> Dracula Pro
-    </h3>
+    <h3>Dracula Pro</h3>
     <p>
       More than a theme. <em>A productivity package</em> for developers.
     </p>
