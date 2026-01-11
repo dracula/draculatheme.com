e7d8f9ea6226efa67eb36e460e4b2e3bf7aade07
Zeno Rocha <zno.rocha@gmail.com>
2026-01-11 14:09:11 -0300

    🔒 add webhook signing

--- src/app/api/inbound/route.ts
+++ src/app/api/inbound/route.ts
@@ -1,46 +1,62 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_TOKEN);
 
 export const POST = async (request: NextRequest) => {
-  const event = await request.json();
+  try {
+    const payload = await request.text();
 
-  if (event.type === 'email.received') {
-    const { data: email } = await resend
-      .emails
-      .receiving
-      .get(event.data.email_id);
-
-    let forwardAddresses = [];
-    
-    switch (email?.to[0]) {
-      case 'support@draculatheme.com':
-        forwardAddresses.push('hello-1@22301188.hubspot-inbox.com');
-        break;
-      case 'zeno@draculatheme.com':
-        forwardAddresses.push('hello-1@22301188.hubspot-inbox.com');
-        forwardAddresses.push('zno.rocha@gmail.com');
-        break;
-      case 'lucas@draculatheme.com':
-        forwardAddresses.push('luxon4uta@gmail.com');
-        break;
-      default:
-        forwardAddresses.push('zno.rocha@gmail.com');
-        break;
-    }
-
-    const { data } = await resend.emails.send({
-      from: 'forward@draculatheme.com',
-      to: forwardAddresses,
-      subject: event.data.subject,
-      html: email?.html || '',
-      text: email?.text || '',
+    const event = await resend.webhooks.verify({
+      payload,
+      headers: {
+        id: request.headers.get("svix-id") || "",
+        timestamp: request.headers.get("svix-timestamp") || "",
+        signature: request.headers.get("svix-signature") || ""
+      },
+      webhookSecret: process.env.RESEND_WEBHOOK_SECRET || ""
     });
 
-    return NextResponse.json(data);
-  }
+    if (event.type === "email.received") {
+      const { data: email } = await resend.emails.receiving.get(
+        event.data.email_id
+      );
 
-  return NextResponse.json({});
+      const forwardAddresses = [];
+
+      switch (email?.to[0]) {
+        case "support@draculatheme.com":
+          forwardAddresses.push("hello-1@22301188.hubspot-inbox.com");
+          break;
+        case "zeno@draculatheme.com":
+          forwardAddresses.push("hello-1@22301188.hubspot-inbox.com");
+          forwardAddresses.push("zno.rocha@gmail.com");
+          break;
+        case "lucas@draculatheme.com":
+          forwardAddresses.push("luxon4uta@gmail.com");
+          break;
+        default:
+          forwardAddresses.push("zno.rocha@gmail.com");
+          break;
+      }
+
+      const { data } = await resend.emails.send({
+        from: "forward@draculatheme.com",
+        to: forwardAddresses,
+        subject: event.data.subject,
+        html: email?.html || "",
+        text: email?.text || ""
+      });
+
+      return NextResponse.json(data);
+    }
+
+    return NextResponse.json({ message: "Email received" });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Invalid webhook", error },
+      { status: 400 }
+    );
+  }
 };
